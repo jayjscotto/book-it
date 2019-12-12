dcaea78f784c8930315c5dde45ba4fb0adedafad
Jason <jayjscotto@gmail.com>
2019-12-12 17:29:47 -0500

    updated bin/www

--- public/javascripts/appointment.js
+++ public/javascripts/appointment.js
@@ -1,12 +1,18 @@
 $(function(e) {
 
-    $(document).on('click', '.appointment-modal', function(e) {
+    //returns array
+    const buttons = document.querySelectorAll('.appointment-modal');
+
+    //assign event listener to each button and get data from API
+    buttons.forEach(button => button.addEventListener('click', e => {
+        e.preventDefault();
+
+        //class ID of facility class button
         const classId = e.target.getAttribute('data-id');
-        console.log('hey')
+        console.log(classId)
         $.get('/api/class-info/' + classId).then((data) => {
             
             console.log(data);
         });;
-    })
-    
+    }));    
 });
