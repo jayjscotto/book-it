fb0ba74097139d527fc0ccba9be91cc3d778c898
Jason <jayjscotto@gmail.com>
2019-12-30 12:42:26 -0500

    updated codebase

--- public/javascripts/view-appts.js
+++ public/javascripts/view-appts.js
@@ -1,40 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const viewAppts = document.querySelector("#view-user-appointments");
+  const viewAppts = $("#view-user-appointments");
 
   if (viewAppts) {
-    viewAppts.addEventListener("click", e => {
+    viewAppts.on("click", e => {
       e.preventDefault();
 
       window.location = "/my-bookfit-appts";
     });
   }
 
-  const cancelAppt = document.querySelectorAll(".cancel-appt");
+  const cancelAppt = $(".cancel-appt");
 
   if (cancelAppt) {
-    cancelAppt.forEach(button => {
-      button.addEventListener("click", e => {
-        e.preventDefault();
+    //cancelAppt.forEach(button => {
+    //button.addEventListener("click", e => {
+    $(".cancel-appt").on("click", e => {
+      e.preventDefault();
 
-        const apptId = button.getAttribute("data-appointment");
+      const apptId = e.target.getAttribute("data-appointment");
 
-        const dataObj = {
-          appt_id: apptId
-        };
+      const dataObj = {
+        appt_id: apptId
+      };
 
-        $.ajax({
-          method: "DELETE",
-          url: "api/user/appt-cancel",
-          data: dataObj
-        }).then(response => {
-          $("#cancel-modal").modal("show");
-          window.location = "/my-bookfit-appts";
-            // $(".cancel-button").on("click", e => {
-            //     e.preventDefault();
-            //     window.location.reload(true);
-            // })
-          
-        });
+      $.ajax({
+        method: "DELETE",
+        url: "api/user/appt-cancel",
+        data: dataObj
+      }).then(response => {
+        $("#cancel-modal").modal("show");
+        window.location = "/my-bookfit-appts";
       });
     });
   }
